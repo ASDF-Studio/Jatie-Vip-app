a93545ad0c829957a3f89c4bf184a528e8c47056
Gaurav Mantrao <developer@airlystudio.com>
2023-03-09 18:31:58 +0530

    - Api baseUrl changes

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,9 +1,9 @@
-export const API_BASE_URL = "https://staging.jatievip.com/";
+export const API_BASE_URL = "https://staging.jatievip.com/api/auth/";
 
 export const API_END_POINTS = {
     LOGIN: "login",
     VERIFY_OTP: "verifyotp",
     CHECK_USERNAME: "getusername",
-    UPDATE_USER: "api/auth/updateuser",
-    UPLOAD_PROFILE_PIC: "api/upload"
+    UPDATE_USER: "updateuser",
+    UPLOAD_PROFILE_PIC: "upload"
 };
